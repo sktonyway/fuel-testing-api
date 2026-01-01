bf777b075422a05f5e5ff8dc2763252f792ed73f
Sagar <sktonyway@gmail.com>
2026-01-01 15:45:52 +0530

    Add JSON parsing and submitTxn route & sorted api txn.
    Added JSON body parsing and a new route for submitting transactions.

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ import Transaction from './transaction.model.js';
 import connectDB from './db.js';
 const app = express();
 app.use(cors());
+app.use(express.json()); // 👈 REQUIRED for req.body
+
+
 await connectDB();
-
-
 app.get('/', (req,res)=>{
     res.send("This shows server is running.")
 })
@@ -29,7 +30,7 @@ app.get('/api/txn',async (req,res)=>{
 app.get('/api/txn/:erv', async (req,res)=>{
     try{
         const {erv} = req.params;
-        const data = await Transaction.find({erv}).sort({ date: 1 });
+        const data = await Transaction.find({erv}).sort({ date: -1 });
         res.json(data)
     }catch(err){
         console.error(err);
@@ -44,6 +45,33 @@ app.get('/api/vehicles', async (req,res)=>{
     } catch (error) {
         console.error(error);
         
+    }
+})
+// This route is for accepting data from frontend.
+app.post('/api/submitTxn', async (req,res)=>{
+    try{
+        const {erv,user,prevOdo,currOdo,totalKm,vol,rate,amt,kmpl,remarks} = req.body;
+        // Creating the transaction object.
+        const txn = new Transaction({
+      erv: erv.trim().toUpperCase(),
+      user: user.trim(),
+      prevOdo: Number(prevOdo),
+      currOdo: Number(currOdo),
+      totalKm: Number(totalKm),
+      vol: Number(vol),
+      rate: Number(rate),
+      amt: Number(amt),
+      kmpl: Number(kmpl),
+      remarks,
+      date: date ? new Date(date) : undefined // fallback to Date.now
+    });
+        const savedTxn = await txn.save(); // Saved to mongodb;
+        res.status(201).json({
+      success: true,
+      data: savedTxn
+    });   
+    }catch(err){
+        console.error("Error in inserting data submit route.😂",err);
     }
 })
 
